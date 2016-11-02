2480c268519bae86c9e6876bcbaa195d9fb6f2cc
adam17 <adam.paulukanis@gmail.com>
2016-11-02 18:24:43 +0000

    Modify moje_skrypty/JanGunka.sh

--- moje_skrypty/JanGunka.sh
+++ moje_skrypty/JanGunka.sh
@@ -5,12 +5,13 @@ ADRES="https://youtu.be/kv6s5n86KkQ?list=PLKGnFgBkzrwKFlqMnFcr3duWlpHqw8lv6";
 
 # sprawdzam, jakie formaty są obsługiwane.
 # Wychodzi na to, że nie każdy film mogę ściągnąć w formacie 249.
-youtube-dl -F ${ADRES}
+#youtube-dl -F ${ADRES}
 
 # Więc użyję tego
 #youtube-dl -f 249/43 ${ADRES} 
 
 # ściągnięte z http://bytefreaks.net/gnulinux/bash/ffmpeg-extract-audio-from-webm-to-mp3
 for FILE in *.webm; do
+  echo "Processing ${FILE}";
   ffmpeg -i "${FILE}" -vn -ab 128k -ar 44100 -y "${FILE%.webm}.mp3";
 done;
